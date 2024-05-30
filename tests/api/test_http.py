dfd1b675da89a6b167976f088881a2f4d907edda
aakhtiamova <168908141+aakhtiamova@users.noreply.github.com>
2024-05-30 11:09:24 +0200

    Update test_http.py
    test http

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -6,3 +6,13 @@ import requests
 def test_first_request ():
     r = requests.get ('https://api.github.com/zen')
     print(f"Response is {r.text}")
+
+
+@pytest.mark.http
+def test_second_request ():
+    r = requests.get ('https://api.github.com/users/defunkt')
+    body = r.json()
+
+    assert body['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    print(f."Response Headers are {r.headers}") 
