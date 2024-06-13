ec6ed4e3654500777e8c545a3b30aecffd4d942a
aakhtiamova <168908141+aakhtiamova@users.noreply.github.com>
2024-06-13 13:22:18 +0200

    Update database.py

--- modules/common/database.py
+++ modules/common/database.py
@@ -1,1 +1,16 @@
+import sqlite3
 
+
+class Database():
+
+  def __init__(self):
+    self.connection = sqlite3.connect( r'/Users/anastasiiaakhtiamova/git' + r'/become_qa_auto.db')
+    self.cursor = self.connection.cursor()
+
+  def test_connection(self):
+    sqlite_select_Query = "SELECT sqlite version();"
+    self.cursor.execute(sqlite_select_Query)
+    record = self.cursor.fetchall()
+    print(f"Connected successfully. SQLite Database Version is: {record}")
+    
+  
